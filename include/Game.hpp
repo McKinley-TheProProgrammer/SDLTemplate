fad217ce4ff8793a017f4b8b48ee70078078b78b
McKinley <caiohudson.red4@hotmail.com>
2024-03-25 00:49:09 -0300

    Added the Game class for the Template

---
+++ include/Game.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include <string.h>
+
+#include "SDL_include.h"
+
+using namespace std;
+
+class Game
+{
+
+private:
+    Game(string title, int window_W, int window_H);
+
+    inline static Game* instance = nullptr;
+
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+
+
+    bool isRunning; //Temp variable
+
+public:
+    ~Game();
+    static Game& Instance();
+
+    void Run();
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,9 @@
 
 #include <iostream>
-#include <SDL.h>
+#include "Game.hpp"
 
 int main(int argv, char** args)
 {
     std::cout << "Hello World!" << std::endl;
-
-
-    int gameInit = SDL_Init(SDL_INIT_EVERYTHING);
-
-    SDL_Window* window = SDL_CreateWindow("This window",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-    800,600, 0);
-
-    SDL_Renderer* renderer = SDL_CreateRenderer(window,-1,0);
-
-    bool isRunning = true;
-    // Event serves for Inputs
-    SDL_Event event;
-
-    while (isRunning)
-    {
-        while(SDL_PollEvent(&event)){
-
-            switch (event.type)
-            {
-            case SDL_QUIT:
-                // Stops the game
-                isRunning = false;
-                break;
-            
-            case SDL_KEYDOWN:
-                if(event.key.keysym.sym == SDLK_ESCAPE)
-                {
-                    isRunning = false;
-                }
-            }
-        }
-
-        SDL_RenderClear(renderer);
-        SDL_SetRenderDrawColor(renderer,200,0,242,255);
-        SDL_RenderPresent(renderer);
-    }
-
-    SDL_RenderClear(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-  
-    return 0;
+    Game::Instance().Run();
 }
